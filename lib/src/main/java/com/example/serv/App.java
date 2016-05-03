1a8b65d7fc77290326139dd7f9f45e672157e7ea
Noel Lin <nolin9889@gmail.com>
2016-05-03 13:52:57 -0700

    WOrkinggit status! message showing

--- lib/src/main/java/com/example/serv/App.java
+++ lib/src/main/java/com/example/serv/App.java
@@ -10,7 +10,7 @@ public class App {
 
         System.out.println("Sending POST to GCM");
 
-        String apiKey = "AIzaSyC_oi5v-QwyHirz_xbDzwcdcAROWrZ-xwc";
+        String apiKey = "AIzaSyAZmj94eahwWwsmUCLP2vhJwbAK3zNt0-k";
         Content content = createContent();
 
         Post2Gcm.post(apiKey, content);
@@ -19,7 +19,7 @@ public class App {
     public static Content createContent() {
         Content c = new Content();
 
-        c.addRegId("APA91bGB-ZbeyCtLP_QRNQE5zu4Y-1LgRlTFiIMh2vDrhtgVwL5vTo5DYiE_YFo4ftOjxk4Nihykt_LSTPL9T_7FX64I7EQoe-l_eQ_4-T36BwAdOgAioktNUE1fwFoaPe6fdzQ1Aayx");
+        c.addRegId("APA91bEnQ6V7jsaCE-ZfnUtRHoM4QyvoDizGl-pusLkTZtCFY_G3BaTQWjaukOOdjMpjSVCCwGV-9Jdl6TScZeSDj_BR0qkosRUYtuBe9_59yHCNk9T5ZWzSvC-Dkhu7wJxFyHXziVVc");
 
         c.createData("Working!!", "Test message");
 
